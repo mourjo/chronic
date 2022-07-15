466c8d4e3ff5840ec0283fba1248d770b8b098a0
Mourjo Sen <hello@mourjo.me>
2022-07-15 09:17:58 +0530

    Add string utilities

---
+++ src/main/java/me/mourjo/chronic/utils/StringUtils.java
@@ -0,0 +1,15 @@
+package me.mourjo.chronic.utils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class StringUtils {
+    private static final Pattern atomSplitter = Pattern.compile("\\s+");
+    public static List<String> tokenize(String expression) {
+        // currently, does not support command args
+        return Arrays.stream(atomSplitter.split(expression.trim(), 8))
+                .map(String::trim).collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/me/mourjo/chronic/utils/StringUtilsTest.java
@@ -0,0 +1,30 @@
+package me.mourjo.chronic.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StringUtilsTest {
+    @Test
+    void tokenizeTest() {
+        assertEquals(List.of(
+                        "*/15",
+                        "0",
+                        "1,15",
+                        "*",
+                        "1-5",
+                        "/usr/bin/find"),
+                StringUtils.tokenize("*/15 0 1,15 * 1-5 /usr/bin/find"));
+
+        assertEquals(List.of(
+                        "*/15",
+                        "0",
+                        "1,15",
+                        "*",
+                        "1-5",
+                        "/usr/bin/find"),
+                StringUtils.tokenize("      */15     0     1,15      *      1-5     /usr/bin/find  "));
+    }
+}
