73759277768d225f680886b107a1bced83aedead
Mourjo Sen <hello@mourjo.me>
2022-07-16 20:28:26 +0530

    Add Minute test

--- src/test/java/me/mourjo/chronic/field/MinuteTest.java
+++ src/test/java/me/mourjo/chronic/field/MinuteTest.java
@@ -1,11 +1,42 @@
 package me.mourjo.chronic.field;
 
+import me.mourjo.chronic.exceptions.UnexpectedAtomException;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MinuteTest {
     @Test
-    void parseMinuteTest() {
-        Minute m = new Minute("*/15");
+    void parseValidMinuteTest() {
+        Minute m = new Minute("0");
+        assertEquals("0", m.toString());
 
+        m = new Minute("59");
+        assertEquals("59", m.toString());
+
+        m = new Minute("*/15");
+        assertEquals("0,15,30,45", m.toString());
+
+        m = new Minute("25-33/15");
+        assertEquals("30", m.toString());
+
+        m = new Minute("25-33,40-59/15");
+        assertEquals("30,45", m.toString());
+
+        m = new Minute("*/2500");
+        assertEquals("0", m.toString());
+    }
+
+    @Test
+    void parseInValidMinuteTest() {
+        Minute m = new Minute("88");
+        assertThrows(UnexpectedAtomException.class, m::toString);
+
+        m = new Minute("25-100/2");
+        assertThrows(UnexpectedAtomException.class, m::toString);
+
+        m = new Minute("1-2,80-90");
+        assertThrows(UnexpectedAtomException.class, m::toString);
     }
 }
